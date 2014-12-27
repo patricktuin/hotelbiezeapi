cae63621b8effa00c23324736d6c5936bcffab7c
Patrick van der Tuin <patricktuin@planet.nl>
2014-12-27 15:24:37 +0100

    version 0.1

--- server.js
+++ server.js
@@ -5,81 +5,112 @@ var bodyParser = require('body-parser');
 
 
 // connect to mongodb
-mongoose.connect('mongodb://localhost/food');
+mongoose.connect('mongodb://localhost/hotelbieze-dev');
 
 // Add headers
 app.use(function (req, res, next) {
 // Website you wish to allow to connect
-res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 // Request methods you wish to allow
-res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 // Request headers you wish to allow
-res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 // Set to true if you need the website to include cookies in the requests sent
 // to the API (e.g. in case you use sessions)
-res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Credentials', true);
 // Pass to next layer of middleware
-next();
+    next();
 });
 
 app.use(bodyParser.json());
 
+// define model for reviews  =================
+var Reviews = mongoose.model('hotelreviews', {
+    stars: {type: Number, required: true},
+    body: String,
+    author: String
+});
 
-  // define model for recipes  =================
-  var Recipes = mongoose.model('Recipes', {
-    name : {type: String, required: true},
-    category : String,
-    description: String,
+// get all reviews
+app.get('/', function (req, res) {
+    Reviews.find(function (err, reviews) {
+        if (err)
+            res.send(err)
+        res.json(reviews);
+
+    });
+});
+
+  // Find one review
+  app.get('/:review_id', function(req, res) {
+      Reviews.find({
+        _id : req.params.review_id
+      },
+        function(err, review) {
+        if (err)
+          res.send(err)
+        res.json(review);
+    });
   });
 
-// get all recipes
-app.get('/', function(req, res) {
-	console.log('GET request');
-	Recipes.find(function(err, recipes){
-		if (err)
-			res.send(err)
-		res.json(recipes);
 
-	});
-});
 
-// post recipe
-app.post('/', function(req, res) {
-	console.log('POST' + JSON.stringify(req.body))
-	Recipes.create({
-		name: req.body.name,
-		category: req.body.category,
-		description: req.body.description,
-	}, function(err, recipes){
-		if (err)
-			res.send(err);
-	})
-		res.send("recipes");
+// post new review
+app.post('/', function (req, res) {
+    console.log('POST' + JSON.stringify(req.body))
+    Reviews.create({
+        stars: req.body.stars,
+        body: req.body.body,
+        author: req.body.author
+    }, function (err, review) {
+        if (err)
+            res.send(err);
+    });
+    res.send("review");
 
 });
 
-// delete recipe
-app.delete('/:recipe_id', function(req, res){
-	Recipes.remove({
-		_id: req.params.recipe_id
-	}, function(err, recipes) {
-		if (err)
-			res.send(err);
-		Recipes.find(function(err, recipes){
-			if (err)
-				res.send(err);
-			res.json(recipes);
-		});
-	});
+// delete review
+app.delete('/:review_id', function (req, res) {
+    Reviews.remove({
+        _id: req.params.review_id
+    }, function (err, review) {
+        if (err)
+            res.send(err);
+
+        
+            Reviews.find(function (err, reviews) {
+        if (err)
+            res.send(err)
+        res.json(reviews);
+
+    });
+});
 });
 
+  // delete a guest
+  app.delete('/api/wedding/:guest_id', function(req, res) {
+    Guests.remove({
+      _id : req.params.guest_id
+    }, function(err, guest) {
+      if (err)
+        res.send(err);
+
+      // get and return all the guests after you create another
+      Guests.find(function(err, guests) {
+        if (err)
+          res.send(err)
+        res.json(guests);
+      });
+    });
+  });
 
 // create webserver
 var server = app.listen(3000, function () {
 
-  var host = server.address().address
-  var port = server.address().port
+    var host = server.address().address
+    var port = server.address().port
 
-  console.log('Example app listening at http://%s:%s', host, port)
+    console.log('hotelbieze api listening at http://%s:%s', host, port)
 
 })
